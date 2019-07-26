7e002139a9e61f90a0e55146568f9e6c58aff32e
Omanshu <omanshu.gupta@aricent.com>
2019-07-26 17:07:21 +0530

    reformatting.

--- charts-app/src/components/chartRender.js
+++ charts-app/src/components/chartRender.js
@@ -1,57 +1,53 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 let a = -1;
-class ChartRender extends Component {
-  
 
-  xAxisLable(){
-     a = a+1;
-     return this.props.processedData[a].date
+class ChartRender extends Component {
+
+  xAxisLable() {
+    a = a + 1;
+    return this.props.processedData[a].date
   }
 
-render (){
-  
-
+  render() {
     const options = {
       chart: {
         type: 'line'
       },
-      yAxis :{
-        title : {
+      yAxis: {
+        title: {
           text: Object.keys(this.props.processedData[0])[1].toUpperCase()
         },
       },
-      xAxis:{
-        title : {
-          text:  Object.keys(this.props.processedData[0])[0].toUpperCase()
-        },
-       categories : this.props.processedData.map(val=>val.date)
-      //   labels :{
-      //        formatter :()=>this.xAxisLable()
-            
-      // }
-    },
+      xAxis: {
         title: {
-          text: `STOCK Line Chart for ${this.props.selectStock}`
+          text: Object.keys(this.props.processedData[0])[0].toUpperCase()
         },
-        series: [
-          {
-            name : `FOR COMPANY - ${this.props.selectStock}`,
-            data: this.props.processedData.map(val=>Number(val.price) )
-}
-        ]
-      };
+        categories: this.props.processedData.map(val => val.date)
+        //   labels :{
+        //        formatter :()=>this.xAxisLable()
+
+        // }
+      },
+      title: {
+        text: `STOCK Line Chart for ${this.props.selectStock}`
+      },
+      series: [
+        {
+          name: `FOR COMPANY - ${this.props.selectStock}`,
+          data: this.props.processedData.map(val => Number(val.price))
+        }
+      ]
+    };
 
     return (<div>
-        <HighchartsReact
-          highcharts={Highcharts}
-          options={options}
-        />
-      </div>)
-}
-
-
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+      />
+    </div>)
+  }
 }
 
 export default ChartRender;
