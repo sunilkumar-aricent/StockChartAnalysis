0b7104c6b339548e05cfe6bb24a532419af5c113
Daya Gupta <dayagupta@Dayas-MacBook-Air.local>
2019-08-07 22:33:29 +0530

    typeahead styles

--- charts-app/src/common/components/typeahead.js
+++ charts-app/src/common/components/typeahead.js
@@ -1,70 +1,51 @@
 import React, {Fragment} from 'react';
-import {AsyncTypeahead, GithubMenuItem} from 'react-bootstrap-typeahead';
+import {asyncContainer, Typeahead} from 'react-bootstrap-typeahead';
 
-class Typeahead extends React.Component {
-    state = {
-      allowNew: false,
-      isLoading: false,
-      multiple: false,
-      options: [],
-    };
+const AsyncTypeahead = asyncContainer(Typeahead);
 
-    selectCompany = (item) => {
-        console.log(item);
-        this.setState({ options:[] });
-        this.props.selectCompany(item);
-    }
-  
-    render() {
-      return (
-        <Fragment>
-          <AsyncTypeahead
-            {...this.state}
-            id="search-typeahead"
-            labelKey="name"
-            minLength={3}
-            onSearch={this._handleSearch}
-            placeholder="Search for a company..."
-            onChange={this.props.selectCompany}
-            // renderMenuItemChildren={(option, props) => {
-            //     return (
-            //         <GithubMenuItem key={option.id} user={option.name} />
-            //     )
-            // }}
-            // renderMenuItemChildren={(options, props) => {
-            //     return options.map((item) => {
-            //         return <li key={item.id}>{item.name}</li>
-            //     })
-            //   }
-            // }
-          />
-          {/* <FormGroup>
-            {this._renderCheckboxes()}
-          </FormGroup> */}
-          <div class="search-result-container">
-              <ul class="search-result">
-                  {
-                    this.state.options.map((item) => {
-                        return <li className="search-result-option" key={item.id} onClick={() => this.selectCompany(item)}>{item.name}</li>
-                    })
-                  }
-              </ul>
-          </div>
-        </Fragment>
-      );
-    }
-  
-    _handleSearch = (query) => {
-      this.setState({isLoading: true});
-        //   makeAndHandleRequest(query)
-      this.props.searchCompany(query, (options) => {
-          console.log(options);
-        this.setState({
-            isLoading: false,
-            options,
-        });
-      });
-    }
+class TypeaheadComponent extends React.Component {
+  state = {
+    allowNew: false,
+    isLoading: false,
+    multiple: false,
+    options: [],
+  };
+
+  selectCompany = (item) => {
+    this.setState({ options:[] });
+    this.props.selectCompany(item);
   }
 
-  export default Typeahead;
+  render() {
+    return (
+      <Fragment>
+        <AsyncTypeahead
+          id="stock-typeahead"
+          isLoading={this.state.isLoading}
+          labelKey="name"
+          onSearch={this._handleSearch}
+          options={this.state.options || []}
+          placeholder="Search for a company..."
+          onChange={(selected) => {
+            console.log(selected);
+            if (selected.length) {
+              this.props.selectCompany(selected[0]);
+            }
+          }}
+        />
+      </Fragment>
+    );
+  }
+
+  _handleSearch = (query) => {
+    this.setState({isLoading: true});
+    this.props.searchCompany(query, (options) => {
+      this.setState({
+          isLoading: false,
+          options,
+      });
+    });
+  }
+}
+
+export default TypeaheadComponent;
